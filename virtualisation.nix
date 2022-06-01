b25073176b6b6e2e588eab6a18c2513c85004583
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-01 15:39:33 +0100

    Add file: virtualisation.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ virtualisation.nix
@@ -0,0 +1,20 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.ovmf.enable = true;
+    qemu.runAsRoot = true;
+    onBoot = "ignore";
+    onShutdown = "shutdown";
+  };
+
+  virtualisation.podman.enable = true;
+  virtualisation.docker.enable = true;
+  virtualisation.waydroid.enable = true;
+  virtualisation.lxd.enable = true;
+}
