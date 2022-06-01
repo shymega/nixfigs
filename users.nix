3061a0d57e6072c364eb0d50b860ce48f60d95cf
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-01 15:39:33 +0100

    Add file: users.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ users.nix
@@ -0,0 +1,24 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  users.users.dzr = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    description = "Rodriguez, Dom";
+    extraGroups = [
+      "wheel"
+      "dialout"
+      "adbusers"
+      "uucp"
+      "kvm"
+      "docker"
+      "lp"
+      "disk"
+      "systemd-journal"
+    ];
+  };
+}
