86b999ac9ab7495033de264d585e2b3763444f3d
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-01 15:39:33 +0100

    Add file: locale.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ locale.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }:
+
+{
+  time.timeZone = "Europe/London";
+
+  console = {
+    keyMap = "uk";
+    font = "Lat2-Terminus16";
+  };
+
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    inputMethod = { enabled = "ibus"; };
+  };
+}
