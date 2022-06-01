8d4155b4bb4c73985d6f4e4d06413144b6de3624
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-01 15:39:33 +0100

    Add file: wayland.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ wayland.nix
@@ -0,0 +1,34 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      waybar
+      wl-clipboard
+      wf-recorder
+      mako
+      slurp
+      grim
+      wofi
+      alacritty
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+
+  programs.waybar.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+}
