48e3ecc136b1c4a581072a59fa90c759ef13b138
Dom Rodriguez <shymega@shymega.org.uk>
2022-07-16 23:07:24 +0100

    Add file: system-services.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ system-services.nix
@@ -0,0 +1,30 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  services = {
+    flatpak.enable = true;
+    thermald.enable = true;
+    openssh = {
+      enable = true;
+      startWhenNeeded = true;
+    };
+    udisks2 = { enable = true; };
+    printing = { enable = true; };
+    avahi = { enable = true; };
+    blueman.enable = true;
+    zerotierone.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+}
