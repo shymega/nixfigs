bff09293a957490318398095decb728ca7d039b0
Dom Rodriguez <shymega@shymega.org.uk>
2022-07-16 23:07:24 +0100

    Add file: packages.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ packages.nix
@@ -0,0 +1,38 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  networking = {
+    networkmanager = { plugins = [ pkgs.networkmanager-openvpn ]; };
+  };
+
+  fonts.fonts = with pkgs; [
+    open-dyslexic
+    fira
+    fira-code
+    font-awesome_5
+    font-awesome_4
+    noto-fonts
+    noto-fonts-emoji
+    emojione
+    twemoji-color-font
+  ];
+
+  nixpkgs.config = {
+    allowBroken = false;
+    android_sdk.accept_license = true;
+    allowUnfree = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    gnupg
+    pinentry
+    acpi
+    openvpn
+    tmux
+    linux_xanmod_latest.dev
+  ];
+}
