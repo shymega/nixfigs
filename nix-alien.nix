9d77d226e868703d77473195ec288d04d1da4f6a
Dom Rodriguez <shymega@shymega.org.uk>
2023-01-12 19:38:57 +0000

    Add file: nix-alien.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nix-alien.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+let
+  nix-alien-pkgs = import
+    (fetchTarball "https://github.com/thiagokokada/nix-alien/tarball/master")
+    { };
+in {
+  programs.nix-ld.enable = true;
+
+  environment.systemPackages = with nix-alien-pkgs; [
+    nix-alien
+    nix-index-update
+    pkgs.nix-index # not necessary, but recommended
+  ];
+}
