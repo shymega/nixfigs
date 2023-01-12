e9ca4a86ea3c0e467ed75ead6fd1e4dc8f581853
Dom Rodriguez <shymega@shymega.org.uk>
2023-01-12 19:38:57 +0000

    Add file: fonts.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ fonts.nix
@@ -0,0 +1,23 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  fonts.fonts = with pkgs; [
+    open-dyslexic
+    fira
+    fira-code
+    jetbrains-mono
+    font-awesome_5
+    font-awesome_4
+    noto-fonts
+    noto-fonts-emoji
+    emojione
+    twemoji-color-font
+    ibm-plex
+  ];
+
+  fonts.fontDir.enable = true;
+}
