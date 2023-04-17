f9762b5508ef0d2d60edc90573f2060d3de7a2be
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-17 20:57:42 +0000

    Remove unnecessary persist directories
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/global/impermanence.nix
+++ common/global/impermanence.nix
@@ -4,12 +4,6 @@
   environment.persistence."/persist" = {
     directories = [
       "/etc/NetworkManager"
-      "/var/lib"
-      "/etc/ssh"
-      "/var/lib/systemd"
-      "/var/lib/nixos"
-      "/var/log"
-      "/srv"
     ];
     files = [ "/etc/machine-id" ];
   };
