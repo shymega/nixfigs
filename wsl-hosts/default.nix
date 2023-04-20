e0dc3f3aac1d2712e808ab450a450827b680bf87
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 23:52:22 +0100

    Add wsl-hosts folder and default.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- wsl-hosts/default.nix
+++ wsl-hosts/default.nix
@@ -1,3 +1,4 @@
-{ lib, inputs, nixpkgs, nixpkgs-unstable, nixos-hardware, impermanence, nix-colors, nix-on-droid, nixos-wsl, sops-nix, nix-alien, emacs-overlay, doom-emacs, home-manager, ... }:
-{
-}
+{ lib, inputs, nixpkgs, nixpkgs-unstable, nixos-hardware, impermanence
+, nix-colors, nix-on-droid, nixos-wsl, sops-nix, nix-alien, emacs-overlay
+, doom-emacs, home-manager, ... }:
+{ }
