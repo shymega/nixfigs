7a199bf4cce6b4f0b58ba944513ed3a466244e11
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 23:37:20 +0100

    Lint impermanence.nix
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/global/impermanence.nix
+++ common/global/impermanence.nix
@@ -2,11 +2,7 @@
   imports = [ inputs.impermanence.nixosModules.impermanence ];
 
   environment.persistence."/persist" = {
-    directories = [
-      "/etc/NetworkManager"
-      "/var/lib"
-      "/root"
-    ];
+    directories = [ "/etc/NetworkManager" "/var/lib" "/root" ];
     files = [ "/etc/machine-id" ];
   };
 }
