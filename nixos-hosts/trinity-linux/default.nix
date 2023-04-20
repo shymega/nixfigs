149fc4e3904ef3022262586183d12c109ed4a8ea
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 23:56:26 +0100

    Add TRINITY-LINUX NixOS host
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nixos-hosts/trinity-linux/default.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, user, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ./wayland.nix ./x11.nix ];
+
+  environment.etc."crypttab".text = ''
+    homecrypt /dev/disk/by-label/HOMECRYPT /persist/etc/.homecrypt.bin
+  '';
+
+  networking.hostName = "TRINITY-LINUX";
+  time.timeZone = "Europe/London";
+
+  boot = {
+    cleanTmpDir = true;
+
+    supportedFilesystems = [ "ntfs" ];
+    extraModprobeConfig = ''
+      options hid_apple fnmode=0
+    '';
+
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    kernelParams = [ "quiet" ];
+
+    initrd.luks.devices = {
+      nixos = {
+        device = "/dev/disk/by-label/NIXOS";
+        preLVM = true;
+        allowDiscards = true;
+      };
+    };
+
+    plymouth = {
+      enable = true;
+      themePackages = with pkgs; [ breeze-plymouth ];
+      theme = "breeze";
+    };
+
+    loader = {
+      systemd-boot = { enable = true; };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      timeout = 3;
+    };
+  };
+}
