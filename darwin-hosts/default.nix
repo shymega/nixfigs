f3f202f1220414fe58e0e2e7160bfcce99835c7a
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 23:36:43 +0100

    Add more imports to darwin-hosts, and lint
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- darwin-hosts/default.nix
+++ darwin-hosts/default.nix
@@ -1,15 +1,16 @@
-{ lib, inputs, nixpkgs, home-manager, nix-darwin, ...}:
+{ lib, inputs, nixpkgs, nixpkgs-unstable, nixos-hardware, impermanence, nix-colors, nix-on-droid, nixos-wsl, sops-nix, nix-alien, emacs-overlay, doom-emacs, home-manager, ... }:
 {
   mac-vm = darwin.lib.darwinSystem {
     system = "x86_64-darwin";
     specialArgs = { inherit inputs; };
     modules = [
-        ./mac-vm/configuration.nix
-      
-      home-manager.darwinModules.home-manager {
+      ./mac-vm/configuration.nix
+
+      home-manager.darwinModules.home-manager
+      {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.users.dzrodriguez = import ../users/home.nix
+        home-manager.users.dzrodriguez = import ../users/home.nix;
       }
     ];
   };
