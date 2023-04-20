70dae91ed6a321605df220bc6792b09750352701
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-20 23:38:28 +0100

    Add wsl-hosts nixconfig, stub
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ wsl-hosts/default.nix
@@ -0,0 +1,3 @@
+{ lib, inputs, nixpkgs, nixpkgs-unstable, nixos-hardware, impermanence, nix-colors, nix-on-droid, nixos-wsl, sops-nix, nix-alien, emacs-overlay, doom-emacs, home-manager, ... }:
+{
+}
