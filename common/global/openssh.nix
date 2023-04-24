dd53941eab8ef8a0e2191a4d507bc39da3b6079f
Dom Rodriguez <shymega@shymega.org.uk>
2023-04-24 22:05:33 +0100

    Enable OpenSSH
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/global/openssh.nix
+++ common/global/openssh.nix
@@ -4,15 +4,6 @@ let inherit (config.networking) hostName;
 in {
   services.openssh = {
     enable = true;
-    settings = {
-      # Harden
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      # Automatically remove stale sockets
-      StreamLocalBindUnlink = "yes";
-      # Allow forwarding ports to everywhere
-      GatewayPorts = "clientspecified";
-    };
   };
 
   # Passwordless sudo when SSH'ing with keys
