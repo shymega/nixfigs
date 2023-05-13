d47e8543bbdee947b20519ac168c0e5a4765fd4c
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-13 01:05:36 +0100

    Switch around 1Password configuration
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -136,16 +136,5 @@ in {
     zoxide
   ];
 
-  # Enable the 1Password CLI, this also enables a SGUID wrapper so the CLI can authorize against the GUI app
-  programs._1password = {
-    enable = true;
-  };
-
-  # Enable the 1Passsword GUI with myself as an authorized user for polkit
-  programs._1password-gui = {
-    enable = true;
-    polkitPolicyOwners = ["dzrodriguez"];
-  };
-
   news.display = "silent";
 }
