0c1adeb792083fd4507b196f112103a82b2d4264
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-13 01:05:36 +0100

    Switch around 1Password configuration

--- nixos-hosts/configuration.nix
+++ nixos-hosts/configuration.nix
@@ -110,5 +110,17 @@
         TimeoutStopSec = 10;
       };
   };
-};
+  
+  };
+
+    # Enable the 1Password CLI, this also enables a SGUID wrapper so the CLI can authorize against the GUI app
+  programs._1password = {
+    enable = true;
+  };
+
+  # Enable the 1Passsword GUI with myself as an authorized user for polkit
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = ["dzrodriguez"];
+  };
 }
