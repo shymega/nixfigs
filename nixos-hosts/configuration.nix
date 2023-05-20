59c4785bdcb622656b608c771104cd230562e2a1
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-20 22:18:34 +0100

    Add polkit agent

--- nixos-hosts/configuration.nix
+++ nixos-hosts/configuration.nix
@@ -106,4 +106,21 @@
   };
 
   networking.firewall.checkReversePath = false;
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 }
