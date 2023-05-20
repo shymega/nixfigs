a38f40ef41e1a764ef2c844e194d35ed59f9524a
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-20 20:52:43 +0100

    Add Thinkpad X270 config
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nixos-hosts/link-linux/default.nix
@@ -0,0 +1,112 @@
+{ pkgs, lib, user, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ./wayland.nix ./x11.nix ];
+
+  networking.hostName = "LINK-LINUX";
+  time.timeZone = "Europe/London";
+
+  boot = {
+    cleanTmpDir = true;
+
+    supportedFilesystems = [ "ntfs" "btrfs" ];
+    extraModprobeConfig = ''
+      options hid_apple fnmode=0
+    '';
+
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
+    kernelParams = [ "quiet" ];
+
+    initrd.luks.devices = {
+      nixos = {
+        device = "/dev/disk/by-label/NIXOS";
+        preLVM = true;
+        allowDiscards = true;
+      };
+    };
+
+    plymouth = {
+      enable = true;
+      themePackages = with pkgs; [ breeze-plymouth ];
+      theme = "breeze";
+    };
+
+    loader = {
+      systemd-boot = { enable = true; };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      timeout = 3;
+    };
+
+    # Note `lib.mkBefore` is used instead of `lib.mkAfter` here.
+    initrd.postDeviceCommands = lib.mkBefore ''
+      mkdir -p /mnt
+
+      # We first mount the btrfs root to /mnt
+      # so we can manipulate btrfs subvolumes.
+      mount -o subvol=/ /dev/disk/by-label/NIXOS_BTRFS_ROOT /mnt
+
+      # While we're tempted to just delete /root and create
+      # a new snapshot from /root-blank, /root is already
+      # populated at this point with a number of subvolumes,
+      # which makes `btrfs subvolume delete` fail.
+      # So, we remove them first.
+      #
+      # /root contains subvolumes:
+      # - /root/var/lib/portables
+      # - /root/var/lib/machines
+      #
+      # I suspect these are related to systemd-nspawn, but
+      # since I don't use it I'm not 100% sure.
+      # Anyhow, deleting these subvolumes hasn't resulted
+      # in any issues so far, except for fairly
+      # benign-looking errors from systemd-tmpfiles.
+      btrfs subvolume list -o /mnt/root |
+      cut -f9 -d' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done &&
+      echo "deleting /root subvolume..." &&
+      btrfs subvolume delete /mnt/root
+
+      echo "restoring blank /root subvolume..."
+      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+      # Once we're done rolling back to a blank snapshot,
+      # we can unmount /mnt and continue on the boot process.
+      umount /mnt
+    '';
+  };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="power_supply", KERNEL=="ADP1", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl --no-block start battery.target"
+    SUBSYSTEM=="power_supply", KERNEL=="ADP1", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl --no-block start ac.target"
+  '';
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+
+  services.tlp = {
+    enable = false;
+    settings = {
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+      START_CHARGE_THRESH_BAT0 = 90;
+      STOP_CHARGE_THRESH_BAT0 = 97;
+      RUNTIME_PM_ON_BAT = "auto";
+    };
+  };
+
+  services.logind = {
+    extraConfig = ''
+      HandleLidSwitchExternalPower=ignore
+      LidSwitchIgnoredInhibited=no
+    '';
+  };
+}
---
+++ nixos-hosts/link-linux/x11.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+let
+  swayConfig = pkgs.writeText "greetd-sway-config" ''
+    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -c sway; swaymsg exit"
+    bindsym Mod4+shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot' \
+      -b 'Suspend' 'systemctl suspend'
+  '';
+in {
+  services.greetd = {
+    enable = true;
+    restart = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
+      };
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    sway
+    startplasma-x11
+    startplasma-wayland
+    cinnamon-session
+    bash
+    fish
+    zsh
+  '';
+
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.startx.enable = true;
+      libinput.enable = true;
+      desktopManager = {
+        cinnamon.enable = true;
+        plasma5.enable = true;
+      };
+      windowManager = { awesome.enable = true; };
+      layout = "us";
+    };
+  };
+}
