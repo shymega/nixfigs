bf15b28d6c542e864f7022385b4e52ad21f0a89f
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-21 17:51:41 +0100

    Make Flake pure again
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/global/common_env.nix
+++ common/global/common_env.nix
@@ -1,10 +1,8 @@
 { inputs, config, lib, pkgs, ... }:
 let
   nixpkgs-unstable = import inputs.nixpkgs-unstable {
-    config = {
-      allowUnfree = true;
-      system = "x86_64-linux";
-    };
+    config = { allowUnfree = true; };
+    system = "x86_64-linux";
   };
 in {
   environment = {
