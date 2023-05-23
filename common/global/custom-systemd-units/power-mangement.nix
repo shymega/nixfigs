d7830d30723a5ba716ea81fd29780e751961a43f
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-24 00:33:27 +0100

    format

--- common/global/custom-systemd-units/power-mangement.nix
+++ common/global/custom-systemd-units/power-mangement.nix
@@ -1,37 +1,40 @@
-{ config, pkgs, lib, ...}:
+{ config, pkgs, lib, ... }:
 
 {
-  systemd.services.power-maximum-tdp = lib.mkIf (config.networking.hostName == "NEO-LINUX") {
-    description = "Change TDP to maximum TDP when on AC power";
-    wantedBy = [ "ac.target" ];
-    unitConfig = { RefuseManualStart = true; };
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart =
-        "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=28000 --fast-limit=28000 --slow-limit=28000 --tctl-temp=90";
+  systemd.services.power-maximum-tdp =
+    lib.mkIf (config.networking.hostName == "NEO-LINUX") {
+      description = "Change TDP to maximum TDP when on AC power";
+      wantedBy = [ "ac.target" ];
+      unitConfig = { RefuseManualStart = true; };
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart =
+          "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=28000 --fast-limit=28000 --slow-limit=28000 --tctl-temp=90";
+      };
     };
-  };
 
-  systemd.services.power-saving-tdp = lib.mkIf (config.networking.hostName == "NEO-LINUX") {
-    description = "Change TDP to power saving TDP when on battery power";
-    wantedBy = [ "battery.target" ];
-    unitConfig = { RefuseManualStart = true; };
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart =
-        "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=8000 --fast-limit=8000 --slow-limit=8000 --tctl-temp=90";
+  systemd.services.power-saving-tdp =
+    lib.mkIf (config.networking.hostName == "NEO-LINUX") {
+      description = "Change TDP to power saving TDP when on battery power";
+      wantedBy = [ "battery.target" ];
+      unitConfig = { RefuseManualStart = true; };
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart =
+          "${pkgs.ryzenadj}/bin/ryzenadj --stapm-limit=8000 --fast-limit=8000 --slow-limit=8000 --tctl-temp=90";
+      };
     };
-  };
 
-  systemd.services.powertop = lib.mkIf (config.networking.hostName == "LUKS-LINUX") {
-    description = "Auto-tune Power Management with powertop";
-    unitConfig = { RefuseManualStart = true; };
-    wantedBy = [ "battery.target" "ac.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.powertop}/bin/powertop --auto-tune";
+  systemd.services.powertop =
+    lib.mkIf (config.networking.hostName == "LUKS-LINUX") {
+      description = "Auto-tune Power Management with powertop";
+      unitConfig = { RefuseManualStart = true; };
+      wantedBy = [ "battery.target" "ac.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.powertop}/bin/powertop --auto-tune";
+      };
     };
-  };
 
   systemd.services."inhibit-suspension@" = {
     description = "Inhibit suspension for one hour";
