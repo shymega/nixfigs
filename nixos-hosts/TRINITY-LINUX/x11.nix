39e1d15bbace889d42f6cd6fcbc849da4b2f3eb5
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-23 20:05:41 +0100

    rotate screen on greetd
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- nixos-hosts/TRINITY-LINUX/x11.nix
+++ nixos-hosts/TRINITY-LINUX/x11.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
   swayConfig = pkgs.writeText "greetd-sway-config" ''
+    output DSI-1 transform 90
     exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
     exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -c sway; swaymsg exit"
     bindsym Mod4+shift+e exec swaynag \
