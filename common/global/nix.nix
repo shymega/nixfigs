e773b1566da3c357e15b944fdb20b1b87c3c998c
Dom Rodriguez <shymega@shymega.org.uk>
2023-05-25 15:52:12 +0100

    Update Nix config

--- common/global/nix.nix
+++ common/global/nix.nix
@@ -1,14 +1,20 @@
 { pkgs, inputs, lib, config, ... }: {
   nix = {
+    optimise.automatic = true;
     settings = {
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
-      warn-dirty = false;
+      warn-dirty = true;
       system-features = [ "kvm" "big-parallel" ];
     };
     package = pkgs.nixVersions.unstable;
     registry.nixpkgs.flake = inputs.nixpkgs;
+    daemonCPUSchedPolicy = "batch";
+    extraOptions = ''
+      gc-keep-outputs = true
+      gc-keep-derivations = true
+    '';
     gc = {
       automatic = true;
       dates = "daily";
