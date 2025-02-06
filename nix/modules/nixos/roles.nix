5a57d5f2436bafdbd711d285e0e7c10e71da6e70
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-06 20:55:26 +0000

    refactor: A new beginning (Snowfall-based approach)
    A new beginning for Nixfigs.
    
    Snowfall Lib based approach!
    
    Changes so far:
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Auto builds and push to Attic (WIP! DO NOT MERGE!)
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nix/modules/nixos/roles.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{
+  lib,
+  hostRoles,
+  ...
+}:
+with lib; {
+  options.nixfigs.meta.rolesEnabled = mkOption {
+    default = hostRoles;
+    type = with types; listOf str;
+  };
+}
