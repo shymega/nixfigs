25c9eaeace29b1a76b4e09cc1eb52d02317c0591
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-07 18:55:38 +0000

    refactor: A new beginning (Snowfall-based approach)
    A new beginning for Nixfigs.
    
    Snowfall Lib based approach!
    
    Changes so far:
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Auto builds and push to Attic (WIP! DO NOT MERGE!)
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/systems/armv6l-linux/DZR-BUSY-LIGHT/hardware-configuration.nix
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
+
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages_rpi0;
+
+    initrd.availableKernelModules = lib.mkForce [
+      "usbhid"
+      "usb_storage"
+      "xhci_pci"
+      "mmc_block"
+    ];
+
+    kernelParams = lib.mkAfter [
+      "8250.nr_uarts=1"
+      "console=ttyAMA0,115200"
+      "console=tty1"
+      "cma=128M"
+      "kunit.enable=0"
+    ];
+
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+}
