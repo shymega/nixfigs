3c68458a8ef17b3d35445aa2e6cb9bf5b8b0a2dc
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-07 18:55:38 +0000

    refactor: A new beginning
    A new beginning for Nixfigs.
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ overlays/stable/overlays.d/nginx-rtmp.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+_final: prev: {
+  nginx-rtmp = prev.nginxStable.override (oldAttrs: {
+    pname = "nginx-rtmp";
+    modules = oldAttrs.modules ++ [prev.nginxModules.rtmp];
+  });
+}
