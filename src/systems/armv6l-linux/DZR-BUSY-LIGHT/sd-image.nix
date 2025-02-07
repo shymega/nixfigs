0cca3245572519a05bad2973da6a0eb20fdd3d5c
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-07 18:55:38 +0000

    refactor: A new beginning (Snowfall-based approach)
    A new beginning for Nixfigs.
    
    Snowfall Lib based approach!
    
    Changes so far:
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Auto builds and push to Attic (WIP! DO NOT MERGE!)
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/systems/armv6l-linux/DZR-BUSY-LIGHT/sd-image.nix
@@ -0,0 +1,52 @@
+{
+  inputs,
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  nixpkgs.crossSystem = lib.systems.elaborate lib.systems.examples.raspberryPi;
+
+  imports = [
+    "${inputs.nixpkgs}//nixos/modules/profiles/base.nix"
+    "${inputs.nixpkgs}//nixos/modules/installer/sd-card/sd-image.nix"
+    ./minification.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    libraspberrypi
+    libgpiod
+    gpio-utils
+    i2c-tools
+    vim
+    tmux
+    git
+  ];
+
+  networking.wireless.enable = true;
+
+  boot = {
+    kernelModules = [
+      "i2c-dev"
+    ];
+  };
+  hardware.i2c.enable = true;
+
+  users = {
+    extraGroups = {
+      gpio = {};
+    };
+  };
+  services.getty.autologinUser = "app";
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  services.udev = {
+    extraRules = ''
+      KERNEL=="gpiochip0*", GROUP="gpio", MODE="0660"
+    '';
+  };
+}
