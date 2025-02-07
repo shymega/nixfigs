f93c43137ab048e6f7e74c2d197e3bb9172468d8
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-07 18:55:38 +0000

    refactor: A new beginning
    A new beginning for Nixfigs.
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/systems/nixosConfigurations/DEUSEX-JOVIAN@x86_64-linux/hardware-configuration.nix
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [./disks.nix];
+
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" "sdhci_pci"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
