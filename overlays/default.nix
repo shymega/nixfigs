66fc77e15af4cef6fe34fb9323906d22fe38ac5f
Dom Rodriguez <shymega@shymega.org.uk>
2025-06-03 00:04:26 +0000

    refactor: A new beginning
    A new beginning for Nixfigs.
    
    - New dummy inputs for work, private. To be replaced in CI and
      overridden.
    - New CI layout.
    - REUSE compliance.
    - Formatting checks.
    - Inputs updates.
    - Dependabot.
    - Mergify.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ overlays/default.nix
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+{
+  lib,
+  inputs,
+  ...
+}: let
+  importStableOverlay = overlay: lib.composeExtensions (_: _: {__inputs = inputs;}) (import (./stable/enabled.d + "/${overlay}"));
+
+  stableOverlays = builtins.readDir ./stable/enabled.d;
+
+  stableOverlaysWithImports =
+    lib.mapAttrs' (
+      overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importStableOverlay overlay)
+    )
+    stableOverlays;
+
+  defaultOverlays = with inputs; [
+    agenix.overlays.default
+    android-nixpkgs.overlays.default
+    deckcheatz.overlays.default
+    dzr-taskwarrior-recur.overlays.default
+    nix-alien.overlays.default
+    nix-doom-emacs-unstraightened.overlays.default
+    nur.overlays.default
+    shypkgs-public.overlays.default
+    xrlinuxdriver.overlays.default
+  ];
+
+  customOverlays = [
+    (import ./shymega {inherit inputs lib;})
+    (import ./unstable {inherit inputs lib;})
+  ];
+in
+  stableOverlaysWithImports
+  // {
+    default = lib.composeManyExtensions (
+      defaultOverlays ++ customOverlays ++ (lib.attrValues stableOverlaysWithImports)
+    );
+  }
---
+++ overlays/shymega/default.nix
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+{
+  inputs,
+  lib,
+  ...
+}: _final: prev: let
+  importShymegaOverlay = overlay: lib.composeExtensions (_: _: {__inputs = inputs;}) (import (./enabled.d + "/${overlay}"));
+
+  shymegaOverlays = lib.mapAttrs' (
+    overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importShymegaOverlay overlay)
+  ) (builtins.readDir ./enabled.d);
+in {
+  shymega = import inputs.nixpkgs-shymega {
+    inherit (prev) system;
+    config = inputs.self.nixpkgs-config;
+    overlays = builtins.attrValues shymegaOverlays;
+  };
+}
