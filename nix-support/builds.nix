22d898f567e4cb6c376c2e80c82487054a44dd10
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-18 17:51:17 +0100

    refactor: modularize monolithic flake configuration
    Break down large flake.nix into focused, reusable
    components to improve maintainability, readability,
    and enable better separation of concerns.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nix-support/builds.nix
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+# Build definitions for SD images and ISO images
+{
+  self,
+  inputs,
+  ...
+}: let
+  inherit (inputs.nixpkgs.lib) hasAttrByPath filterAttrs;
+in {
+  builds = {
+    sdImages = with builtins;
+      mapAttrs (_: v: v.config.system.build.sdImage)
+      (filterAttrs (_: v:
+        hasAttrByPath ["config" "system" "build" "sdImage"] v)
+      self.nixosConfigurations);
+    isoImages = with builtins;
+      mapAttrs (_: v: v.config.system.build.isoImage)
+      (filterAttrs (_: v:
+        hasAttrByPath ["config" "system" "build" "isoImage"] v)
+      self.nixosConfigurations);
+  };
+}
