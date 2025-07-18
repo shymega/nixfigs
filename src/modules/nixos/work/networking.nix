eabdada6218d7bba13f3f309d8dc478426bf1db3
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-18 17:51:19 +0100

    refactor: clean up work module structure
    Reorganize work module components for better maintainability
    and remove redundant configuration blocks.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/modules/nixos/work/networking.nix
+++ src/modules/nixos/work/networking.nix
@@ -7,25 +7,6 @@ let
   inherit (lib) checkRoles;
 in {
   config = lib.mkIf (checkRoles ["work"] config) {
-    # Work-specific network configurations
-    networking.networkmanager.ensureProfiles.profiles = {
-      "Corporate-WiFi" = {
-        connection = {
-          id = "Corporate-WiFi";
-          type = "wifi";
-          autoconnect = true;
-        };
-        wifi = {
-          ssid = "CORP-SECURE";
-          security = "wpa-psk";
-        };
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "@WORK_WIFI_PSK@"; # From secrets
-        };
-      };
-    };
-
     # Block personal services when in work mode
     networking.firewall.extraCommands = ''
       # Block Syncthing on work systems
@@ -36,12 +17,6 @@ in {
       iptables -A OUTPUT -d dropbox.com -j DROP
       iptables -A OUTPUT -d drive.google.com -j DROP
     '';
-
-    # Corporate DNS settings
-    networking.nameservers = [
-      "8.8.8.8" # Replace with corporate DNS
-      "8.8.4.4"
-    ];
 
     # Disable IPv6 if required by corporate policy
     networking.enableIPv6 = lib.mkDefault false;
