d1b39b82ea8da9ba80bce40da5a919e135d3ecd1
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-18 17:51:17 +0100

    refactor: establish clean foundation for major refactor
    Create clean baseline configuration structure to
    support comprehensive refactoring effort with
    improved modularity and maintainability.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ overlays/default.nix
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+{
+  lib,
+  inputs,
+  ...
+}:
+with lib; let
+  importStableOverlay = overlay: composeExtensions (_: _: {__inputs = inputs;}) (import (./stable/enabled.d + "/${overlay}"));
+
+  stableOverlays = builtins.readDir ./stable/enabled.d;
+
+  stableOverlaysWithImports =
+    mapAttrs' (
+      overlay: _: nameValuePair (removeSuffix ".nix" overlay) (importStableOverlay overlay)
+    )
+    stableOverlays;
+
+  defaultOverlays = with inputs; [
+    agenix.overlays.default
+    android-nixpkgs.overlays.default
+    deckcheatz.overlays.default
+    dzr-taskwarrior-recur.overlays.default
+    nix-alien.overlays.default
+    nix-doom-emacs-unstraightened.overlays.default
+    nur.overlays.default
+    shypkgs-public.overlays.default
+    xrlinuxdriver.overlays.default
+  ];
+
+  customOverlays = [
+    (import ./shymega {inherit inputs lib;})
+    (import ./unstable {inherit inputs lib;})
+  ];
+in
+  stableOverlaysWithImports
+  // {
+    default = composeManyExtensions (
+      defaultOverlays ++ customOverlays ++ (attrValues stableOverlaysWithImports)
+    );
+  }
