6ec35bbe818dd0c10688d312fb1da9f230c1948c
Dom Rodriguez <shymega@shymega.org.uk>
2026-07-27 17:49:28 +0100

    chore: Uprev hyprland-plugins

--- flake.nix
+++ flake.nix
@@ -207,7 +207,7 @@
     # Desktop environment
     hyprland.follows = "hyprnix/hyprland";
     hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins?ref=v0.55.0";
+      url = "github:hyprwm/hyprland-plugins?ref=v0.56.0";
       inputs.hyprland.follows = "hyprland"; # Prevents version mismatch.
     };
     split-monitor-workspaces = {
