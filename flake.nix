471570da9560ca4b1cda35afaef55fb3827cc723
Dom Rodriguez <shymega@shymega.org.uk>
2026-09-01 01:04:59 +0200

    chore: Remove `lanzaboote` from inputs

--- flake.nix
+++ flake.nix
@@ -152,10 +152,6 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote?ref=v1.1.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     # Development tools
     devenv = {
